cc535fc7ef29eeb37435f5d8d3c411dcb147c2fd
Jason Brunelle <jasonmb626@gmail.com>
2024-11-06 08:29:59 -0600

    Remove reocurring meetings from bootup. Just keep them there permanently

--- bin/DR_Bootup.sh
+++ bin/DR_Bootup.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-id=$(task add +\#CST proj:mtg.cust.nucleus.blocker sched:sod+8.5h unt:sched+1h "$(date +%Y-%m-%d) - Nucleus Blocker" | grep -Po '\d+')
-task add proj:org.time eff:1m depends:$id "$(date +%Y-%m-%d) - Nucleus Blocker INC"
-task add +\#CST proj:mtg.cust.jtf sched:sod+10.75h unt:sched+1h "$(date +%Y-%m-%d) - JTF"
-id=$(task add +\#CST proj:mtg.cust.nucleus.scrum sched:sod+13h unt:sched+1h "$(date +%Y-%m-%d) - Nucleus Scrum" | grep -Po '\d+')
-task add proj:org.time eff:1m depends:$id "$(date +%Y-%m-%d) - Nucleus Scrum INC"
+task add proj:org.time eff:1m "$(date +%Y-%m-%d) - Nucleus Blocker INC"
+task add proj:org.time eff:1m "$(date +%Y-%m-%d) - Nucleus Scrum INC"
 task add proj:org.time eff: 5m sched:sod+8h unt:sched+12h "$(date +%Y-%m-%d) - Enter timesheet"
-task add proj:org.DR eff:1m sched:sod+8h unt:sched+8h "$(date +%Y-%m-%d) - Add all other meetings to task list"
+task add proj:org.DR eff:1m sched:sod+8h unt:sched+8h "$(date +%Y-%m-%d) - Add today's non-recurring meetings to task list"
 task add proj:org.DR eff:5m sched:sod+8h unt:sched+8h "Review WAITING taks. Have any blockers cleared?"
 task add proj:org.DR eff:5m sched:sod+8h unt:sched+8h "Assign proper projects to anything in the inbox"
 task add proj:org.DR eff:1m sched:sod+8h unt:sched+8h "Review \"On My Radar\" list"
 task add proj:org.DR eff:1m sched:sod+8h unt:sched+8h "$(date +%Y-%m-%d) - Transfer notes from phone list"
-task add +\#RPT proj:org.DR eff:30m sched:sod+8h unt:sched+8h "$(date +%Y-%m-%d) - Daily Review"
 task add proj:org.email sched:sod+8h unt:sched+8h "Review TCS Email"
 task add +\#EML proj:org.email sched:sod+8h unt:sched+8h "$(date +%Y-%m-%d) - Email 0"
 
